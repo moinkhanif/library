4d2b0798d6162dd4493a3375b03348eebe224d3a
Moin Khan <kmoin786@gmail.com>
2020-07-21 20:56:55 +0530

    Add basic new book prompt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+document.getElementById('new-book').addEventListener('click', ()=> {
+  let newBook = prompt("Please enter the book name:");
+  if(newBook != null){
+    addBooksToLibrary(newBook);
+    alert('New Book has been added!');
+  }
+})
 
+let myLibrary = [];
+
+let Book = () => {
+
+}
+let addBooksToLibrary = (bookName) => {
+  myLibrary.push(bookName);
+}
